451bc01004f4e59a2113eb6996b7ef76abed7b74
Valteir <valteir.ea@hotmail.com>
2020-02-09 21:08:55 -0300

    sessão e autenticação

--- src/app/controladores/base-controlador.js
+++ src/app/controladores/base-controlador.js
@@ -1,15 +1,49 @@
+const LivroControlador = require('./livro-controlador');
+
 const templates = require('../views/templates');
 
 class BaseControlador {
 
     static rotas() {
         return {
-            home: '/'
+            home: '/',
+            login: '/login'
         }
     }
 
     home() {
         return (req, resp) => resp.marko(templates.base.home);        
+    }
+
+    login() {
+        return (req, resp) => resp.marko(templates.base.login);
+    }
+
+    efetuaLogin() {
+        return (req, resp, next) => {
+
+            const passport = req.passport;
+            passport.authenticate('local', (erro, usuario, info) => {
+
+                if (info) {
+                    return resp.marko(templates.base.login);
+                }
+
+                if (erro) {
+                    return next(erro);
+                }
+
+                req.login(usuario, (erro) => {
+                    if (erro) {
+                        return next(erro);
+                    }
+
+                    return resp.redirect(LivroControlador.rotas().lista);
+                });
+
+            }) (req, resp, next);
+
+        }
     }
 
 
